2952a31db4ed02191404c4505398714f4f727b0a
MishkaM17 <teletubis2000@wp.pl>
2019-06-09 11:31:36 +0200

    Poczatek powiadomien

--- app/src/main/java/com/example/wemiftalk/Utils/SendNotification.java
+++ app/src/main/java/com/example/wemiftalk/Utils/SendNotification.java
@@ -9,7 +9,7 @@ public class SendNotification {
 
     public SendNotification (String message, String heading, String notificationKey){
 
-        notificationKey = "5af1c589-b96c-4d3f-8d2a-a4ecd0539671";
+        notificationKey = "5af1c589-b96c-4d3f-8d2a-a4ecd0539671"; // id mojego nr do wyslania probnego powiadomienia
         try {
 
             JSONObject notificationContent = new JSONObject("{'contents' :{'en' : '" + message + "'}," +
